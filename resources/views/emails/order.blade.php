ed996f9278fe4b73fdad12be5c4ff975157e00d7
Нилов Вадим Валерьевич <vnilov@ctcmedia.ru>
2017-03-17 17:41:31 +0300

    mail fix

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -1,5 +1,5 @@
 <table>
-    <tr><th>Номер телефона</th><td>{{ $phone }}</td></tr>
-    <tr><th>Имя</th><td>{{ $name }}</td></tr>
-    <tr><th>Предпочтительное время</th><td>{{ $time }}</td></tr>
+    <tr><th align="left">Номер телефона</th><td>{{ $phone }}</td></tr>
+    <tr><th align="left">Имя</th><td>{{ $name }}</td></tr>
+    <tr><th align="left">Предпочтительное время</th><td>{{ $time }}</td></tr>
 </table>
